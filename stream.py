dfdbeaaafaf1d71bdf7b774b60197197acace48b
Enrique Sanz Gonzalez <enrique.sanz.glez@gmail.com>
2023-01-21 14:49:16 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -7,5 +7,8 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('Fruit List')
+
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect('pick';my_fruit_list.index)
 streamlit.dataframe(my_fruit_list)
