012f4bd88253b9682b41d04471aa67ad096cff18
Enrique Sanz Gonzalez <enrique.sanz.glez@gmail.com>
2023-01-21 14:37:46 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title('Healthy Dinner')
-
-
-streamlit.header('Recipes')
-streamlit.text('cabbage soup')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
