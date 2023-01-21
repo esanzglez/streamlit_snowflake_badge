608ed48041d57038300925cce22065f58302f74a
Enrique Sanz Gonzalez <enrique.sanz.glez@gmail.com>
2023-01-21 14:52:08 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -10,5 +10,6 @@ streamlit.header('Fruit List')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect('pick',my_fruit_list.Fruit)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect('pick',my_fruit_list.index)
 streamlit.dataframe(my_fruit_list)
