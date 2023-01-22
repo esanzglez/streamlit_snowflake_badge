3a71887505a843c9f8807c888d67670a4709a27a
Enrique Sanz Gonzalez <enrique.sanz.glez@gmail.com>
2023-01-22 09:48:01 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -17,6 +17,6 @@ fruits_selected = streamlit.multiselect('pick',my_fruit_list.index,['Grapes'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
