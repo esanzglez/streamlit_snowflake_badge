b240f681a0a278ae79c6720195854073ebe72c8b
Enrique Sanz Gonzalez <enrique.sanz.glez@gmail.com>
2023-01-22 11:27:51 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -26,3 +26,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/{}".format(
 
 table1 = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(table1.T)
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
