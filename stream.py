802ba0262eff6d131b3fa44b7224f435526c4fc3
Enrique Sanz Gonzalez <enrique.sanz.glez@gmail.com>
2023-01-22 10:01:08 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -19,7 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit = st.text_input("Name a Fruit", 'Kiwi')
+fruit = streamlit.text_input("Name a Fruit", np.null)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/{}".format(fruit))
 
 table1 = pandas.json_normalize(fruityvice_response.json())
